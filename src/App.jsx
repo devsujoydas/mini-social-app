22b92fae44b16cd8cc6f2e09aece505c1eabed1e
devsujoydas <devsujoydas@gmail.com>
2025-04-30 23:15:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <div className='flex justify-center items-center min-h-screen'>
+        <h1 className='text-2xl font-semibold'>Hello i am mini-social-app</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
