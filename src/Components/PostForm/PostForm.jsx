5748a20c447fed8951042773933cf2484136aa48
devsujoydas <devsujoydas@gmail.com>
2025-05-15 23:01:58 +0600

    change input for img file

--- src/Components/PostForm/PostForm.jsx
+++ src/Components/PostForm/PostForm.jsx
@@ -10,25 +10,28 @@ const PostForm = () => {
 
       <form action="">
         <div className="flex w-full items-start gap-2">
-          <ImAttachment className="text-2xl text-zinc-400" />
+          <label for="dropzone-file" className=" text-3xl p-2 rounded-full cursor-pointer active:scale-95 transition-all hover:bg-zinc-200">
+            <ImAttachment className="text-2xl text-zinc-400 cursor-pointer " />
+          <input type="file" id="dropzone-file" className="hidden" />
+          </label>
           <textarea name="" id="" className=" outline-none p-2 w-full" placeholder="Whats on your mind right now?"></textarea>
 
         </div>
 
 
         <div className="flex items-center justify-end gap-2 ">
-          <button className="border border-zinc-400 text-2xl p-3 rounded-full cursor-pointer active:scale-95 transition-all hover:bg-zinc-200">
+          <label  className="border border-zinc-400 text-2xl p-3 rounded-full cursor-pointer active:scale-95 transition-all hover:bg-zinc-200">
             <FaRegSmile />
-          </button>
+          </label>
 
-          <button className="border border-zinc-400 text-3xl p-2 rounded-full cursor-pointer active:scale-95 transition-all hover:bg-zinc-200">
+          <label  className="border border-zinc-400 text-3xl p-2 rounded-full cursor-pointer active:scale-95 transition-all hover:bg-zinc-200">
             <IoMicOutline />
-          </button>
+          </label>
 
 
-          <button className="lg:w-fit flex items-center gap-2 bg-blue-700 hover:bg-blue-600 text-white text-center px-6 py-3 rounded-full cursor-pointer active:scale-95 transition-all">
+          <label  className="lg:w-fit flex items-center gap-2 bg-blue-700 hover:bg-blue-600 text-white text-center px-6 py-3 rounded-full cursor-pointer active:scale-95 transition-all">
             <input type="submit" value={"Post"} className="" /><VscSend className="text-2xl" />
-          </button>
+          </label>
         </div>
       </form>
     </div>
