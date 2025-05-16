68df2d916a25c32daa7028628b18205330dc91e3
devsujoydas <devsujoydas@gmail.com>
2025-05-16 22:45:51 +0600

    add img

--- src/Components/ProfileSidebar/ProfileSidebar.jsx
+++ src/Components/ProfileSidebar/ProfileSidebar.jsx
@@ -125,7 +125,7 @@ const ProfileSidebar = () => {
           <div className="flex justify-between items-center">
             <div className="flex items-center gap-3">
               <div className="active:scale-95 transition-all cursor-pointer">
-                <img className="w-10 h-10 rounded-full" src="https://scontent.fdac99-1.fna.fbcdn.net/v/t39.30808-6/476776223_608657025121384_459306901201601229_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGmzj5MzrRPtlztI2x7LUHcUlgl-gkt_VhSWCX6CS39WP8-nfI6bdrO4XJaRPo3zy1x95_sgj9hrDDBsLNUJSg4&_nc_ohc=0hCIR33ducgQ7kNvwFyY7Fk&_nc_oc=AdnIsTV9QxbiG7ctywkHngKpAJkXf74BbaQ-dSJBmYPQpGbqgpVPyKX0BLEDo0zBpSw&_nc_zt=23&_nc_ht=scontent.fdac99-1.fna&_nc_gid=vA3F32gQ1f87OG0SS9mNqQ&oh=00_AfKnKCJbnKTu2DKRCxHKZwFBkWVqEvgXytiXER4g47o97w&oe=6820E049" alt="" />
+                <img className="w-10 h-10 rounded-full" src="/ahadul.jpg" alt="" />
               </div>
               <div>
                 <h1 className="font-semibold active:underline transition-all cursor-pointer">Ahadul Islam</h1>
@@ -141,7 +141,7 @@ const ProfileSidebar = () => {
           <div className="flex justify-between items-center">
             <div className="flex items-center gap-3">
               <div className="active:scale-95 transition-all cursor-pointer">
-                <img className="w-10 h-10 rounded-full" src="https://scontent.fdac99-1.fna.fbcdn.net/v/t39.30808-6/474783706_1148459610184954_2782533247798226751_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFMBEX8SX0jSTkut3zU25ZmdpTVMep4FFd2lNUx6ngUV5TCaMrUHz9DjcFU8BypO375t3JNhZ0QHZTvbuhLoZTK&_nc_ohc=YK1DOLfP1FUQ7kNvwHCtIZ9&_nc_oc=AdnyNqnDd-ApiQo4Cig4z3UBltipuDWGsHi80uCqF3jaytYy_VWyz7pLt6bciSRTcl0&_nc_zt=23&_nc_ht=scontent.fdac99-1.fna&_nc_gid=UEmrC8DtgD40Mf8jO1cOdA&oh=00_AfKQ6ulyS6tLXGjjBE_aon-tPhA1t_ApVcypwGrto24B8w&oe=6820D9CA" alt="" />
+                <img className="w-10 h-10 rounded-full" src="/maksudur.jpg" alt="" />
               </div>
               <div>
                 <h1 className="font-semibold active:underline transition-all cursor-pointer">Maksudur Rahman</h1>
@@ -157,7 +157,7 @@ const ProfileSidebar = () => {
           <div className="flex justify-between items-center">
             <div className="flex items-center gap-3">
               <div className="active:scale-95 transition-all cursor-pointer">
-                <img className="w-10 h-10 rounded-full" src="https://scontent.fdac99-1.fna.fbcdn.net/v/t39.30808-1/481467792_122199383672129698_4209241168445682482_n.jpg?stp=c92.5.1321.1321a_dst-jpg_s200x200_tt6&_nc_cat=109&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeHg_yQ9I_Id6wOSm3XT4Tpmp8nImeV2gSCnyciZ5XaBIJ-IcefUFnDMvzHIIGxL5ABCafyofrPLuC_YgtMT4zV2&_nc_ohc=-eGa2AGEI7kQ7kNvwEg2lbT&_nc_oc=Adn-ND6uOp-IcnPCrVt4QIONibQkKVSVRecMDDwuQvsGpVCCU-Qi2Zl5CoclVN3Guuw&_nc_zt=24&_nc_ht=scontent.fdac99-1.fna&_nc_gid=bGkDxLm2sVpvp9CumVIXjQ&oh=00_AfJ4EH0kC_vP_9LtarwN4nhwJw39_eJXEc28k8VNQEPJrg&oe=6820E6D2" alt="" />
+                <img className="w-10 h-10 rounded-full" src="/Enamul.jpg" alt="" />
               </div>
               <div>
                 <h1 className="font-semibold active:underline transition-all cursor-pointer">Enamul Hoque</h1>
